175e249efcd2082dea6861cbdfc81438df8f9a40
LHJE <theeluke@gmail.com>
2021-01-05 10:29:38 -0700

    Add another test.  Still shows that the elsif is where the changes are needed

--- test/snail_test.rb
+++ test/snail_test.rb
@@ -5,11 +5,22 @@ require './lib/snail'
 class SnailThingTest < Minitest::Test
   def test_three_by_three
     arrayMatrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-]
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]
+                  ]
 
     assert_equal [1,2,3,6,9,8,7,4,5], SnailThing.path(arrayMatrix)
+  end
+
+  def test_four_by_four
+    arrayMatrix = [
+                    [1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10, 11, 12],
+                    [13, 14, 15, 16]
+                  ]
+
+    assert_equal [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10], SnailThing.path(arrayMatrix)
   end
 end
