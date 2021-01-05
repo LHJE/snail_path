abf65b29268eb7787d6231d6f49db9f94ffe2bc8
LHJE <theeluke@gmail.com>
2021-01-05 10:34:16 -0700

    Fixing note typo

--- lib/snail.rb
+++ lib/snail.rb
@@ -9,7 +9,7 @@ class SnailThing
         if element == original_array[-1]
           return_array << element.reverse
           array.pop
-        elsif # This is where the next steps need to be taken, somehow using the next steps need to be taken, Very likely using the array_length
+        elsif # This is where the next steps need to be taken, Very likely using the array_length
           # require "pry"; binding.pry
           return_array << element[-1]
           element.pop
